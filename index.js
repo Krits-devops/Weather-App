b0d754044af872406107aec921598a82e3dfe494
Krits-devops <kritagyarm9@gmail.com>
2024-11-23 17:41:06 -0500

    Added search functionality
    Added search functionality to weather app, including event listener and API call to fetch weather data

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const userInfo = document.querySelector(".user-info-container");
 let oldTab = userTab;
 const API_KEY = "3f6faa3c9bdde6b1b4c64dac4f7c9d8e";
 oldTab.classList.add("current-tab");
+getfromSessionStorage();
 
 function switchTab(newTab){
     if(newTab != oldTab){
         oldTab.classList.remove("current-tab");
-        oldTab = clickedTab;
+        oldTab = newTab;
         oldTab.classList.add("current-tab");
 
          if(!searchForm.classList.contains("active")){
@@ -56,13 +57,14 @@ function getfromSessionStorage(){
     const {lat , lon} = coordinates;
     grantAccessContainer.classList.remove("active");
     loadingScreen.classList.add("active");
+    console.log(lat,lon);
 
     //API Call
 
     try{
-    const response  = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}
-        &appid=${API_KEY}&units=metric`);
+    const response  = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
     const data = await response.json();
+    console.log("success")
     loadingScreen.classList.remove("active");
     userInfo.classList.add("active")
     renderWeatherInfo(data);
@@ -71,6 +73,7 @@ function getfromSessionStorage(){
 
         loadingScreen.classList.remove("active");
         console.log("error");
+        console.log("Issue in API")
 
     }
 }
@@ -87,7 +90,7 @@ function renderWeatherInfo(weatherInfo){
 
     //fetch values from WeatherInfo Object and put it in UI Elements
 
-    cityName.innerText = weatherInfo?.cityName;
+    cityName.innerText = weatherInfo?.name;
     countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
     desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
@@ -109,7 +112,7 @@ function getLocation(){
 function showPosition(position){
     const userCoordinates = {
         lat: position.coords.latitude,
-        lon: position.coords.longtitude,
+        lon: position.coords.longitude,
     }
 
     sessionStorage.setItem("user-coordinates", JSON.stringify(userCoordinates));
@@ -119,6 +122,39 @@ function showPosition(position){
 const grantAccessButton = document.querySelector("[data-grantAccess]");
 
 grantAccessButton.addEventListener("click", getLocation);
+
+//search function in the weather app
+
+let searchInput = document.querySelector("[data-searchInput]");
+
+searchForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let cityName = searchInput.value;
+    if(cityName === "") 
+        return;
+    else
+    fetchSearchWeatherInfo(cityName);
+})
+
+ async function fetchSearchWeatherInfo(city){
+    loadingScreen.classList.add("active");
+    userContainer.classList.remove("active");
+    grantAccessContainer.classList.remove("active");
+
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}
+            &appid=${API_KEY}&units=metric`);
+            const data = await response.json();
+            loadingScreen.classList.remove("active");
+            userContainer.classList.add("active");
+
+            renderWeatherInfo(data);
+
+    }
+    catch(error){
+
+    }
+}
 
 
 
